eb259d159a65a6e98e77dd45a9e91a5d83ac1301
hamisigad71 <hamisigad77@gmail.com>
2026-03-28 22:10:58 +0300

    Set light mode as default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="font-sans h-full overflow-hidden flex flex-col">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <AuthProvider>
             <ActionProvider>
               <TooltipProvider>{children}</TooltipProvider>
